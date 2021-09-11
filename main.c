b2dcf1b95a22a1725203ac169d4ec01f2dec23af
leo78450 <harpiemangue37@gmail.com>
2021-09-11 19:55:43 +0200

    Add gestion priorite v1

--- main.c
+++ main.c
@@ -1,101 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 
-float addition (const float op1, const float op2)
-{
-	return op1 + op2;
-}
+typedef struct Number {
+    char operator;
+    float number;
+} NUMBER;
 
-float soustraction (const float op1, const float op2)
-{
-	return op1 - op2;
-}
+void reorder_list(NUMBER list[], float res, int *i, int *nb_c) {
+    if (*i == 0) {
+        list[0].operator = '+';
+        list[0].number = res;
 
-float multiplication (const float op1, const float op2)
-{
-	return op1 * op2;
-}
-
-float division (const float op1, const float op2)
-{
-	return op1 / op2;
-}
-
-void division2 (const float op1, const float op2)
-{
-	int opp1 = (int)op1;
-  	int opp2 = (int)op2;
-  	int c = 0;
-  
-  	if (opp2 > opp1) printf("Quotien : 0, Reste : %d\n",  opp2);
-  
-  	while (opp1 >= opp2)
-       	{
-    		opp1 = opp1 - opp2;
-    		c++;
- 	 }
-  
-  	printf("Quotien : %d, Reste : %d\n", c, opp1);
-}
-
-float power (const float op1, const float op2)
-{
-	return pow(op1, op2);
-}
-
-void calcul (float op1, float op2, char op)
-{
-	switch(op)
-        {
-                case '+':
-                        printf("Result addition : %f\n", addition(op1, op2));
-			break;
-		case '-':
-                        printf("Result soustraction : %f\n", soustraction(op1, op2));
-             		break;
-	     	case '/':
-			printf("Result divi : %f\n", division(op1, op2));
-              		break;
-	      	case '%':
-                        division2(op1, op2);
-			break;
-                case '*':
-                case 'm':
-                case 'x':
-                        printf("Result multiplication : %f\n", multiplication(op1, op2));
-           		break;
-	   	case '^':
-                        printf("Result power : %lf\n", power(op1, op2));
-              		break;
-	      	default:
-                        printf("Wrong operator !\n");
-			exit(1);
+        for (int j = 1; j < *nb_c-1; ++j) {
+            list[j].operator = list[j+1].operator;
+            list[j].number = list[j+1].number;
         }
+    } else {
+        list[(*i)-1].operator = '+';
+        list[(*i)-1].number = res;
+
+        for (int j = *i; j < *nb_c-1; ++j) {
+            list[j].operator = list[j+1].operator;
+            list[j].number = list[j+1].number;
+        }
+    }
+
+    // Decrease loop cursor
+    (*i)--;
+    (*nb_c)--;
 }
 
-int main (int argc, char **argv)
-{
-	float op1, op2;
-  
-// Check if enough argument, 4 because we take at least 2 number and 1 operator
-	if (argc < 4)
-	{
-		printf("Not enough args !\n");
-		exit(1);
-	}
-	
-	if (argc%2 != 1)
-	{
-		printf("Wrong args number!\n");
-		exit(1);
-	}
+int main() {
+    int nb_c = 5;
+    NUMBER list[nb_c];
 
-	op1 = atof(argv[1]);
-	op2 = atof(argv[3]);
+    list[0].operator = '+';
+    list[0].number = 2;
 
-	calcul(op1, op2, argv[2][0]);
+    list[1].operator = '+';
+    list[1].number = 4;
 
-	return 0;
+    list[2].operator = 'x';
+    list[2].number = 4;
+
+    list[3].operator = '/';
+    list[3].number = 2;
+
+    list[4].operator = '+';
+    list[4].number = 2;
+
+    // x and /
+    for(int i = 1; i < nb_c; i++) {
+
+        float res = -1;
+        if (list[i].operator == 'x') {
+            if (i == 0) {
+                res = list[i].number * list[i+1].number;
+            } else {
+                res = list[i].number * list[i-1].number;
+            }
+        }
+        if (list[i].operator == '/') {
+            if (i == 0) {
+                res = list[i].number / list[i+1].number;
+                //printf("op1:%f op2:%f res:%f", list[i].number, list[i+1].number, res);
+            } else {
+                res = list[i-1].number / list[i].number;
+                //printf("op1:%f op2:%f res:%f", list[i].number, list[i+1].number, res);
+            }
+        }
+
+        if (res != -1) {
+            reorder_list(list, res, &i, &nb_c);
+        }
+    }
+
+    for(int i = 1; i < nb_c; i++) {
+
+        float res = -1;
+        if (list[i].operator == '+') {
+            if (i == 0) {
+                res = list[i].number + list[i+1].number;
+            } else {
+                res = list[i].number + list[i-1].number;
+            }
+        }
+        if (list[i].operator == '-') {
+            if (i == 0) {
+                res = list[i].number - list[i+1].number;
+            } else {
+                res = list[i-1].number - list[i].number;
+            }
+        }
+
+        if (res != -1) {
+            reorder_list(list, res, &i, &nb_c);
+        }
+    }
+
+    // First number of list is the result
+    printf("Result : %f\n", list[0].number);
+
+    return 0;
 }
