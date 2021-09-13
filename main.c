10ff69501fde6249e9b26ddb06f98f303cc5979b
leo78450 <harpiemangue37@gmail.com>
2021-09-13 21:33:59 +0200

    Add use arguments and management of wrong arguments

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef struct Number {
     char operator;
@@ -19,24 +22,81 @@ void reorder_list(NUMBER list[], float res, int *i, int *nb_c) {
     (*nb_c)--;
 }
 
-int main(void) {
-    int nb_c = 5;
+int verifArg(int argc, char **argv)
+{
+	int i;
+	if (strcmp(argv[1],"+") == 0 || strcmp(argv[1],"-") == 0 || strcmp(argv[1],"x") == 0 || strcmp(argv[1],"m") == 0 || strcmp(argv[1],"*") == 0 || strcmp(argv[1],"/") == 0 || strcmp(argv[1],"%") == 0 || strcmp(argv[1],"v") == 0 || strcmp(argv[1],"V") == 0 || strcmp(argv[1],"^") == 0)
+        {
+		//if is - 2 + 2
+                for (i = 1; i < argc; i += 2)
+		{
+			if ((strcmp(argv[i],"+") != 0 || strcmp(argv[i],"-") != 0 || strcmp(argv[i],"x") != 0 ||strcmp(argv[i],"m") != 0 || strcmp(argv[i],"*") != 0 || strcmp(argv[i],"/") != 0|| strcmp(argv[i],"%") != 0 || strcmp(argv[i],"v") != 0 || strcmp(argv[i],"V") != 0 || strcmp(argv[i],"^") != 0) && isdigit(atof(argv[i + 1])) == 0)
+			{
+				printf("Error usage : the the format of the calculation is not up to standard\nExample of usage : \n# 2 + 2 / 3\n# -4 x 3 / 6\n");
+				exit(1);
+			}
+            	}
+		return 2;
+	}
+        else if (strcmp(argv[2],"+") == 0 || strcmp(argv[2],"-") == 0 || strcmp(argv[2],"x") == 0 ||strcmp(argv[2],"m") == 0 || strcmp(argv[2],"*") == 0 || strcmp(argv[2],"/") == 0 || strcmp(argv[2],"%") == 0 || strcmp(argv[2],"v") == 0 || strcmp(argv[2],"V") == 0 || strcmp(argv[2],"^") == 0)
+        {
+	    	//if is 2 + 2
+		for (i = 2; i < argc; i += 2)
+                {
+                        if ((strcmp(argv[i],"+") != 0 || strcmp(argv[i],"-") != 0 || strcmp(argv[i],"x") != 0 ||strcmp(argv[i],"m") != 0 || strcmp(argv[i],"*") != 0 || strcmp(argv[i],"/") != 0 || strcmp(argv[i],"%") != 0 || strcmp(argv[i],"v") != 0 || strcmp(argv[i],"V") != 0 || strcmp(argv[i],"^") != 0) && isdigit(atof(argv[i + 1])) != 0)
+                        {
+                                printf("Error usage : the the format of the calculation is not up to standard\nExample of usage : \n# 2 + 2 / 3\n# -4 x 3 / 6\n");
+                                exit(1);
+                       	}
+		}
+		return 1;
+        }
+        else
+        {
+                printf("Error usage : the the format of the calculation is not up to standard\nExample of usage : \n# 2 + 2 / 3\n# -4 x 3 / 6\n");
+                exit(1);
+            }
+}
+
+int main(int argc, char **argv) {
+
+    int nb_c = argc / 2;
+    int j = verifArg(argc, argv);
+    int i;
+
     NUMBER list[nb_c];
-
-    list[0].operator = '+';
-    list[0].number = 2;
-
-    list[1].operator = '+';
-    list[1].number = 4;
-
-    list[2].operator = 'x';
-    list[2].number = 4;
-
-    list[3].operator = '/';
-    list[3].number = 2;
-
-    list[4].operator = '+';
-    list[4].number = 2;
+    nb_c = 0;
+    //Sort arguments in list
+    for (i = 1; i < argc; i += 2)
+    {
+	    if (j == 1)
+	    {
+	    	// if is 2 + 2
+		if (i == 1)
+		{
+			list[nb_c].operator = '+';
+                	list[nb_c].number = atof(argv[i]);
+			nb_c ++;
+		}
+		else
+		{
+			list[nb_c].operator = argv[i - 1];
+                        list[nb_c].number = atof(argv[i]);
+			nb_c ++;
+		}
+	    }
+	    else if (j == 2)
+	    {
+		// if is - 2 + 2
+		list[nb_c].operator = argv[i];
+                list[nb_c].number = atof(argv[i + 1]);
+                nb_c ++;
+	    }
+	    else
+	    {
+		printf("Error usage : the the format of the calculation is not up to standard\nExample of usage : \n# 2 + 2 / 3\n# -4 x 3 / 6\n");
+	    }
+    }
 
     // Loop to calculate all multiplication and division
     for(int i = 1; i < nb_c; i++) {
